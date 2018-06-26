26308c737c266901115fcf696ad1d6d4d465d072
Gaurav Gandhi <Gaurav.Gandhi@stonybrook.edu>
2018-06-26 12:06:07 +0530

    Basics Part 02

---
+++ 02Basics/marvels.js
@@ -0,0 +1,40 @@
+//  // datatype - Array
+//
+//  const superHeroes = ['Iron Man', 'Spiderman', 'Capt. America','Thor']
+//
+//  console.log(superHeroes);
+//  console.log(superHeroes[0]);
+//  console.log(superHeroes.length);
+//
+// console.log(`We have ${superHeroes.length} super heroes`);
+
+
+const numbers = ['One', 'Two', 'Three', 'Four', 'Five', 'Six']
+
+
+// numbers[1] = 'Something'
+//
+// console.log(numbers)
+
+
+//start
+
+// numbers.shift()
+// console.log(numbers)
+
+// numbers.unshift('something')
+// console.log(numbers)
+
+//end
+
+// console.log(numbers.pop())
+// console.log(numbers)
+
+// numbers.push('Seven')
+// console.log(numbers)
+
+
+//middle
+
+numbers.splice(2, 2, 'something')
+console.log(numbers)
---
+++ 02Basics/scope.js
@@ -0,0 +1,30 @@
+// let iAmGlobal = 'someValue'
+//
+// if (true) {
+//     var iAmLocal = 'someMoreValue'
+//     iAmGlobal = 'superman'
+//     console.log(iAmGlobal);
+//     console.log(iAmLocal);
+// }
+//
+// console.log(iAmLocal)
+// console.log(iAmGlobal)
+
+
+
+// Kings Problem
+
+//let king = 'John'
+
+if (true) {
+  //  let king = 'Sam'
+
+    if (true) {
+        king = 'Ram'
+        console.log(king);
+    }
+}
+
+if (true) {
+  console.log(king);
+}
